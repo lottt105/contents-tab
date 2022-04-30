f70bad0ca3b1af74c6b2e1e60ba85b10b557692c
lottt105 <hyobin0601@naver.com>
2022-04-30 21:20:19 +0900

    선택된 탭에 class(active)적용

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-console.log('app');
-console.log(popularContents);
-console.log(viewContents);
+// console.log(popularContents);
+// console.log(viewContents);
 console.log(recentContents);
+
+const $nav = document.querySelectorAll('.nav li');
+// const $list = document.querySelectorAll('#list div');
+
+for(let i=0; i<$nav.length; i++){
+    $nav[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        // const tabTarget = e.target.getAttribute('href').replace('#', "");
+        // const tabContents = (new Function("return "+tabTarget+`Contents`))();
+        // const $tabTarget = document.getElementById(tabTarget);
+
+        for(let j=0; j<$nav.length; j++){
+            $nav[j].classList.remove('active');
+            // $list[j].style.display = 'none';
+        }
+        e.target.classList.add('active');
+        
+        // $tabTarget.style.display = 'block';
+        // $tabTarget.innerHTML = tabContents[0].title;
+    });
+}
+
+
+
