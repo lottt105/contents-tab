b0b8873e2fed57d1ba63eddf1f315a2508b93427
lottt105 <hyobin0601@naver.com>
2022-05-01 17:04:59 +0900

    class 수정 & 더보기 클릭 구현
    class(active)가 a 태그에 삽입되던 것을 li 태그에 삽입하는 걸로 수정,
    더보기 버튼 클릭 시  해당 json 결과 20개 표시

--- app.js
+++ app.js
@@ -1,63 +1,62 @@
-// console.log(popularContents);
-// console.log(viewContents);
-// console.log(recentContents);
-
 const $nav = document.querySelectorAll('.nav li');
 const $list = document.querySelectorAll('#list div');
+const $moreBtn = document.querySelector('#moreBtn');
 
-let recentList = "";
-let viewList = "";
-let popularList = "";
+const makeContentList = (end) => {
+    let recentList = "";
+    let viewList = "";
+    let popularList = "";
 
-const list10 = () => {
-    for(let i=0; i<10; i++){
+    for(let i=0; i<end; i++){
         recentList += 
-        `<div class='cover'>
-            <div>
-                <span>${recentContents[i].cp}</span><br/>
-                <strong OnClick="location.href='${recentContents[i].url}'">
-                    ${recentContents[i].title}
-                </strong>
-            </div>
-            <div OnClick="location.href='${recentContents[i].url}'">
-                <img src='${recentContents[i].img}'>
-            </div>
-        </div><hr>`;
+            `<div class='cover'>
+                <div>
+                    <span>${recentContents[i].cp}</span><br><br>
+                    <strong OnClick="location.href='${recentContents[i].url}'">
+                        ${recentContents[i].title}
+                    </strong>
+                </div>
+                <div OnClick="location.href='${recentContents[i].url}'">
+                    <img src='${recentContents[i].img}'>
+                </div>
+            </div><hr>`;
         viewList += 
-        `<div class='cover'>
-            <div>
-                <span>${viewContents[i].cp}</span><br/>
-                <strong OnClick="location.href='${viewContents[i].url}'">
-                    ${viewContents[i].title}
-                </strong>
-            </div>
-            <div OnClick="location.href='${viewContents[i].url}'">
-                <img src='${viewContents[i].img}'>
-            </div>
-        </div><hr>`;
+            `<div class='cover'>
+                <div>
+                    <span>${viewContents[i].cp}</span><br><br>
+                    <strong OnClick="location.href='${viewContents[i].url}'">
+                        ${viewContents[i].title}
+                    </strong>
+                </div>
+                <div OnClick="location.href='${viewContents[i].url}'">
+                    <img src='${viewContents[i].img}'>
+                </div>
+            </div><hr>`;
         popularList += 
-        `<div class='cover'>
-            <div>
-                <span>${popularContents[i].cp}</span><br/>
-                <strong OnClick="location.href='${popularContents[i].url}'">
-                    ${popularContents[i].title}
-                </strong>
-            </div>
-            <div OnClick="location.href='${popularContents[i].url}'">
-                <img src='${popularContents[i].img}'>
-            </div>
-        </div><hr>`;
+            `<div class='cover'>
+                <div>
+                    <span>${popularContents[i].cp}</span><br><br>
+                    <strong OnClick="location.href='${popularContents[i].url}'">
+                        ${popularContents[i].title}
+                    </strong>
+                </div>
+                <div OnClick="location.href='${popularContents[i].url}'">
+                    <img src='${popularContents[i].img}'>
+                </div>
+            </div><hr>`;
     }
-    
     $list[0].innerHTML = recentList;
     $list[1].innerHTML = viewList;
     $list[2].innerHTML = popularList;
 };
 
-const showLoading = (func) =>{
+const showLoading = (func, end) =>{
     $list[3].style.display = 'block';
+    $moreBtn.style.display = 'none';
     setTimeout(() => {
-        func();
+        if (end!=0) func(end);
+        else func();
+        $moreBtn.style.display = 'block';
         $list[3].style.display = 'none';
     },1000);
 };
@@ -66,20 +65,23 @@ for(let i=0; i<$nav.length; i++){
     $nav[i].addEventListener('click', (e) => {
         e.preventDefault();
         const tabTarget = e.target.getAttribute('href').replace('#', "");
-        // const tabContents = (new Function("return "+tabTarget+`Contents`))();
         const $tabTarget = document.getElementById(tabTarget);
 
         for(let j=0; j<$nav.length; j++){
             $nav[j].classList.remove('active');
             $list[j].style.display = 'none';
         }
-        e.target.classList.add('active');
+        e.target.parentNode.classList.add('active');
+        makeContentList(10);
 
-        showLoading(()=>{$tabTarget.style.display = 'block';})
-        
-        // $tabTarget.innerHTML = tabContents[0].title;
+        showLoading(() => {$tabTarget.style.display = 'block';}, 0);
     });
 }
 
-showLoading(list10);
+$moreBtn.addEventListener('click', () => {
+    showLoading(makeContentList, 20);
+    setTimeout(() => {$moreBtn.style.display = 'none'}, 1000);
+});
+
+showLoading(makeContentList, 10);
 
